f4ab00a18c7ffbf87fb2a8c0223117f31fc7b23d
Nwelde <nathanweldegiorgis0@gmail.com>
2024-07-14 14:28:22 -0400

    new commit

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,9 +1,25 @@
-// src/services/supabaseClient.js
+
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://aufrdxctzmpaczegvyeu.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1ZnJkeGN0em1wYWN6ZWd2eWV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA5MDAxMjQsImV4cCI6MjAzNjQ3NjEyNH0.l60Fvw7n8Fzg04VCB-tEfowuNwicvkkkWi-hfRqgdE8';
+const supabaseUrl = 'https://aufrdxctzmpaczegvyeu.supabase.co'; // Replace with your actual Supabase URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF1ZnJkeGN0em1wYWN6ZWd2eWV1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA5MDAxMjQsImV4cCI6MjAzNjQ3NjEyNH0.l60Fvw7n8Fzg04VCB-tEfowuNwicvkkkWi-hfRqgdE8'; // Replace with your actual Supabase anon key
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
